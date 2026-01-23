fa7df9bc284cd2ded5eddd1f9ad393ae9351b37b
SeqSEE <SeqSEE@users.noreply.github.com>
2026-01-22 21:11:23 -0500

    update to let sd send messages

---
+++ src/message.h
@@ -0,0 +1,12 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+#include <Arduino.h>
+#include "debug.h"
+typedef struct
+{
+    char body[128];
+    LogLevel_t level = LOG_NONE;
+} Message_t;
+
+static const int MSG_QUEUE_LEN = 16;
+#endif
